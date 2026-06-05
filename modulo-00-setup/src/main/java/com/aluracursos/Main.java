c400a03acb5f16c32c0921dad706f91ec78bc603
Pierre Oscco <lpierre.dev@gmail.com>
2026-06-04 19:15:15 -0500

    modulo-00: hola-mundo funcionando

---
+++ modulo-00-setup/src/main/java/com/aluracursos/Main.java
@@ -0,0 +1,8 @@
+package com.aluracursos;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("¡Hola, LiterAlura!");
+        System.out.println("Java version: "+System.getProperty("java.version"));
+    }
+}
